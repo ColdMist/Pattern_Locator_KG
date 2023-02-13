3c93e54b9488185a3370fc48977a1c379dc4c52d
yinan <st171243@stud.uni-stuttgart.de>
2023-02-13 17:02:04 +0100

    update 2.13

--- temporal_pattern_lookout/evaluation.py
+++ temporal_pattern_lookout/evaluation.py
@@ -14,23 +14,47 @@ import matplotlib.ticker as ticke
 # TODO: 2. for certain relation, count conclusion in test set and premise in train set.
 
 
-def plot_distribution(on='symmetric'):
-    stat_path = '../results/icews14/statistics'
-    temp_path = stat_path + '/dynamic/{}/'.format(on)
-    static_path = stat_path + '/static/{}/'.format(on)
+def plot_train_test_distribution(dataset):
 
-    ploter = PlotTools()
-    for file_ord in [temp_path, static_path]:
-        data_path = list()
-        for file in os.listdir(file_ord):
-            if file[-3:] == 'csv':
-                data_path.append(file)
+    train_set = pd.read_csv('../results/%s/pattern sets/train/stat_rel.csv' % dataset)
+    test_set = pd.read_csv('../results/%s/pattern sets/test/stat_rel.csv' % dataset)
+    train_t_set = pd.read_csv('../results/%s/pattern sets/train/stat_t_rel.csv' % dataset)
+    test_t_set = pd.read_csv('../results/%s/pattern sets/test/stat_t_rel.csv' % dataset)
 
-        test, train = pd.read_csv(file_ord + data_path[0], index_col=0), pd.read_csv(file_ord + data_path[1], index_col=0)
-        ploter.plot_distribution_rel(train, test, showall=True
-                                     , dynamic=True if file_ord == temp_path else False
-                                     , on=on
-                                     , save_path=file_ord)
+    for s in ['temporal', '']:
+        data_train = train_t_set if s == 'temporal' else train_set
+        data_test = test_t_set if s == 'temporal' else test_set
+        rel_num_tr = data_train.loc[:, 'number'].sum()
+        rel_num_te = data_test.loc[:, 'number'].sum()
+
+        data_train.set_index('relation', inplace=True)
+        data_test.set_index('relation', inplace=True)
+        fig, ax = plt.subplots(2, 1, figsize=(24, 6), sharex=True)
+        ax[0].bar(data_train.index, data_train.loc[:, 'number'],
+                  label='number of %s relation in train set' % s, color='#f9766e', edgecolor='grey', alpha=0.5)
+        ax[0].bar(data_test.index, data_test.loc[:, 'number'],
+                  label='number of %s in test set' % s, color='#00bfc4', edgecolor='grey', alpha=0.5)
+
+        ax[1].bar(data_train.index, data_train.loc[:, 'number']/rel_num_tr,
+                  label='percentage of %s relation in train set' % s, color='#f9766e', edgecolor='grey', alpha=0.5)
+        ax[1].bar(data_test.index, data_test.loc[:, 'number']/rel_num_te,
+                  label='percentage of %s in test set' % s, color='#00bfc4', edgecolor='grey', alpha=0.5)
+
+        ax[0].set_xlabel('relations', fontsize=12)
+        ax[1].set_xlabel('relations', fontsize=12)
+        ax[0].set_ylabel('number', fontsize=12)
+        ax[1].set_ylabel('percentage', fontsize=12)
+        for i in range(2):
+            ax[i].tick_params(axis='x', length=0, rotation=30)
+            ax[i].grid(axis='y', alpha=0.5, ls='--')
+            ax[i].legend(frameon=False)
+
+        fig.suptitle('({}) {} relation distribution'.format(dataset, s))
+        plt.xticks([])
+
+        save_path = '../results/{}/statistics/'.format(dataset)
+        plt.savefig(save_path + '%s_Distribution.png' % s, dpi=300)
+        plt.show()
 
 
 def pattern_analyse(dataset, pattern):
@@ -103,44 +127,39 @@ def conclusion_premise_paar(dataset, pattern):
         stat.to_csv(save_path + '/{}.csv'.format(pattern))
 
 
-def plot_distribution(dataset, pattern, by='freq'):
+def plot_distribution(dataset, pattern):
     for t in ['train', 'test']:
         typ = 'dynamic' if pattern.split()[0] == 'temporal' else 'static'
         data = pd.read_csv('../results/{}/statistics/{}/{}/freq_{}.csv'.format(dataset, t, typ, pattern))
         fig, ax = plt.subplots(2, 1, figsize=(24, 6), sharex=True)
-        if by == 'freq':
-            ax[0].bar(data.loc[:, 'relation'].apply(str), data.loc[:, 'number of relation'],
-                   label='number of relation', color='#f9766e', edgecolor='grey', alpha=0.5)
-            ax[0].bar(data.loc[:, 'relation'].apply(str), data.loc[:, 'number of %s' % pattern],
-                   label='number of %s' % pattern, color='#00bfc4', edgecolor='grey', alpha=0.5)
-            ax[1].bar(data.loc[:, 'relation'].apply(str), data.loc[:, 'percentage'],
-                   label='percentage', color='#7B68EE', edgecolor='grey', alpha=0.7)
 
-            ax[0].set_xlabel('relations', fontsize=12)
-            ax[1].set_xlabel('relations', fontsize=12)
-            ax[0].set_ylabel('number', fontsize=12)
-            ax[1].set_ylabel('percentage', fontsize=12)
-            for i in range(2):
-                ax[i].tick_params(axis='x', length=0, rotation=30)
-                ax[i].grid(axis='y', alpha=0.5, ls='--')
-                ax[i].legend(frameon=False)
-                # ax.set_title('{} {}'.format('Dynamic' if dynamic else 'Static', on))
+        ax[0].bar(data.loc[:, 'relation'].apply(str), data.loc[:, 'number of relation'],
+               label='number of relation', color='#f9766e', edgecolor='grey', alpha=0.5)
+        ax[0].bar(data.loc[:, 'relation'].apply(str), data.loc[:, 'number of %s' % pattern],
+               label='number of %s' % pattern, color='#00bfc4', edgecolor='grey', alpha=0.5)
+        ax[1].bar(data.loc[:, 'relation'].apply(str), data.loc[:, 'percentage'],
+               label='percentage', color='#7B68EE', edgecolor='grey', alpha=0.7)
 
-            fig.suptitle('({}) {}-{}-{}'.format(dataset, t, typ, pattern))
-            plt.xticks([])
-            # fig.legend(data.loc[:5, 'relation'])
-            save_path = '../results/{}/statistics/{}/{}/'.format(dataset, t, typ)
-            plt.savefig(save_path + '%s_Distribution.png' % pattern, dpi=300)
-            plt.show()
+        ax[0].set_xlabel('relations', fontsize=12)
+        ax[1].set_xlabel('relations', fontsize=12)
+        ax[0].set_ylabel('number', fontsize=12)
+        ax[1].set_ylabel('percentage', fontsize=12)
+        for i in range(2):
+            ax[i].tick_params(axis='x', length=0, rotation=30)
+            ax[i].grid(axis='y', alpha=0.5, ls='--')
+            ax[i].legend(frameon=False)
+
+        fig.suptitle('({}) {}-{}-{}'.format(dataset, t, typ, pattern))
+        plt.xticks([])
+
+        save_path = '../results/{}/statistics/{}/{}/'.format(dataset, t, typ)
+        plt.savefig(save_path + '%s_Distribution.png' % pattern, dpi=300)
+        plt.show()
 
 
 def main(dataset, pattern):
-    # plot
-    # for on in ['symmetric', 'relations']:
-    #     plot_distribution(on=on)
-
+    plot_train_test_distribution(dataset)
     # pattern_analyse(dataset, pattern)
-    conclusion_premise_paar(dataset, pattern)
-
+    # conclusion_premise_paar(dataset, pattern)
     # plot_distribution(dataset, pattern)
 
